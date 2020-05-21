6093f146cdb7c89504b5733951bb8212bde82eb1
karishma <kkunwar211@gmail.com>
2020-05-21 09:05:46 +0545

    Indentation and semicolon added

--- models/healthpost.js
+++ models/healthpost.js
@@ -3,58 +3,42 @@ const mongoose = require("mongoose");
 
 const HealthPostSchema = new mongoose.Schema({
 
-    name:{
-        type:String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    location:{
+    location: {
         type: String,
         required: true
     },
 
-   
     employee: [{
-        
-     
 
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        
-        
-     
-
-
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }],
-   
 
- 
-
-    
-medicine: [{
-    
+    medicine: [{
 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Medicine'
-    
-}],
 
+    }],
 
-    gps_location :{'type': {
-        type: String,
-        required: false,
-        enum: ['Point']
-        
-      },
-      coordinates: [Number],
-      select: false
+    gps_location: {
+        'type': {
+            type: String,
+            required: false,
+            enum: ['Point']
+
+        },
+        coordinates: [Number],
+        select: false
     },
 
-},{
+}, {
 
     timestamps: true
+});
 
-
-
-})
-
-module.exports = mongoose.model('HealthPost',HealthPostSchema);
+module.exports = mongoose.model('HealthPost', HealthPostSchema);
