d716bbade9ae7076d66f8dfdbf64c0fdf247397e
karishma <kkunwar211@gmail.com>
2020-05-26 19:44:34 +0545

    Clean code added to healthPostRouter

--- routes/healthPostRouter.js
+++ routes/healthPostRouter.js
@@ -1,109 +1,106 @@
 
-const express = require('express');
 const Healthpost = require('../models/healthpost');
-const authenticate = require('../authenticate');
 const cors = require('./cors');
+const express = require('express');
 const healthPostRouter = express.Router();
 const bodyparser = require('body-parser');
-
-const mongoose = require('mongoose');
-const config =require('../config');
-
-
+// const mongoose = require('mongoose');
+// const authenticate = require('../authenticate');
 
 healthPostRouter.use(bodyparser.json());
 
-healthPostRouter.options('*', cors.corsWithOptions, (req, res) => {
+healthPostRouter.route('/')
+  .options(cors.corsWithOptions, (req, res) => {
     res.sendStatus(200);
+  })
+
+  .get(cors.corsWithOptions, async (req, res, next) => {
+    Healthpost.find({})
+      .populate('medicine')
+      .then((healthPost) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(healthPost);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
+  })
+
+
+  .post(cors.corsWithOptions, async (req, res, next) => {
+
+    Healthpost.create(req.body)
+
+      .then((healthPost) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(healthPost);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
+
+  })
+
+
+  .delete('/', cors.corsWithOptions, async (req, res, next) => {
+
+    Healthpost.remove({})
+      .then((healthPost) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          status: true,
+          message: 'Successfully deleted'
+        });
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
+
   });
 
 
+healthPostRouter.route('/:healthpostId')
+
+  .get(cors.corsWithOptions, async (req, res, next) => {
+
+    Healthpost.findOne({ _id: req.params.healthpostId })
+      .populate('medicine')
+      .then((healthPost) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(healthPost.medicine);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
+  })
+
+  .put('/:healthpostId', cors.cors, (req, res, next) => {
+    const medicines = {
+      medicine: req.body.medicine
+    }
+    Healthpost.findByIdAndUpdate({ _id: req.params.healthpostId }, medicines)
+      .then((medicineResult) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(medicineResult);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  })
 
 
-
-healthPostRouter.get('/',cors.corsWithOptions,async(req,res,next) => {
-
-  
-
-          Healthpost.find({})
-          .populate('medicine')
-          .then((healthPost)=>{
-            res.statusCode =200;
-            res.setHeader('Content-Type', 'application/json');
-				res.json(healthPost);
-			}, (err) => next(err))
-			.catch((err) => next(err));
-         
-    })
-          
-
-healthPostRouter.post('/',cors.corsWithOptions,async(req,res,next) =>{
-
-          
-            Healthpost.create(req.body)
-            
-              .then((healthPost)=>{
-                res.statusCode =200;
-                res.setHeader('Content-Type', 'application/json');
-            res.json(healthPost);
-          }, (err) => next(err))
-          .catch((err) => next(err));
-              
-          
-          })
+  .delete('/:healthpostId', cors.cors, (req, res, next) => {
+    Healthpost.findOne({ _id: req.params.healthpostId }).remove('medicine')
+      .then((medicine) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+          status: true,
+          message: 'Successfully deleted'
+        });
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  });
 
 
-
-   healthPostRouter.delete('/',cors.corsWithOptions,async(req,res,next) => {
-
-  
-    Healthpost.remove({})
-			.then((healthPost) => {
-				res.statusCode = 200;
-				res.setHeader('Content-Type', 'application/json');
-				res.json({
-					status: true,
-					message: 'Successfully deleted'
-				});
-			}, (err) => next(err))
-			.catch((err) => next(err));
-
-
-   })
-
-
-  
-
-  healthPostRouter.get('/:healthpostId',cors.corsWithOptions,async(req,res,next) => {
-
-  
-
-    Healthpost.findOne({_id:req.params.healthpostId})
-    .populate('medicine')
-    .then((healthPost)=>{
-      res.statusCode =200;
-      res.setHeader('Content-Type', 'application/json');
-  res.json(healthPost.medicine);
-}, (err) => next(err))
-.catch((err) => next(err));
-   
-})
-
-healthPostRouter.delete('/:healthpostId',cors.cors, (req, res, next) => {
-  Healthpost.findOne({_id: req.params.healthpostId}).remove(Healthpost.medicine)
-    .then((medicine) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({
-        status: true,
-        message: 'Successfully deleted'
-      });
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
-
-
-
-  
 
 module.exports = healthPostRouter;
