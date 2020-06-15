110e73c8b7d1ae654fd1305c5cba46293e69d252
karishma <kkunwar211@gmail.com>
2020-06-15 13:57:42 +0545

    beforeEach hooks in bloodRouter test file

--- test/bloodRouter.js
+++ test/bloodRouter.js
@@ -1,7 +1,17 @@
 process.env.NODE_ENV = 'test';
 
-let HealthPost = require('../models/blood');
+let Blood = require('../models/blood');
 var chai = require('chai');
 var server = require('../app');
 var chaiHTTP = require('chai-http');
 var should = chai.should();
+
+describe('Blood', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        Book.remove({}, (err) => {
+            done();
+        });
+    });
+})
+
+
