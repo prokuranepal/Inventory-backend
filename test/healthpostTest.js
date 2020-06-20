163e3d61d85094998dcc7d2644496f6df5aa04fe
karishma <kkunwar211@gmail.com>
2020-06-20 23:02:28 +0545

    test for post,update,delete the records in healthpost

--- test/healthpostTest.js
+++ test/healthpostTest.js
@@ -23,7 +23,7 @@ describe('Healthpost', () => {
 
     beforeEach(function (done) {
 
-        healthPost1.save(function(err){
+        healthPost1.save(function (err) {
             done();
         });
     })
@@ -43,10 +43,105 @@ describe('Healthpost', () => {
 
             })
     });
-    
+    it('it should  POST healthpost information', (done) => {
+        const healthpost = {
+            name: "Baidhya healthpost",
+            location: 'xyz',
+            employee: healthPost1.employee,
+            medicine: healthPost1.medicine,
+            gps_location: 'tyu89'
+        }
+        chai.request(server)
+            .post('/healthpost')
+            .send(healthpost)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('location');
+                res.body.should.have.property('employee');
+                res.body.should.have.property('medicine');
+                res.body.should.have.property('gps_location');
+                done();
+            });
+    });
 
 
+    it('it should GET information about healthpost by the given id', (done) => {
+        const healthpost = new HealthPost({
+            name: "Baidhya healthpost",
+            location: 'xyz',
+            employee: healthPost1.employee,
+            medicine: healthPost1.medicine,
+            gps_location: 'tyu89'
+        });
+        healthpost.save((err, healthpost) => {
+            chai.request(server)
+                .get('/healthpost/' + healthpost.id)
+                .send(healthpost)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('location');
+                    res.body.should.have.property('employee');
+                    res.body.should.have.property('medicine');
+                    res.body.should.have.property('gps_location');
+                    res.body.should.have.property('_id').eql(healthpost.id);
+                    done();
+                });
+        });
 
+
+    });
+
+    it('it should UPDATE information of healthpost given the id', (done) => {
+        let healthpost = new HealthPost({
+            name: "siddhi smriti",
+            location: 'Bhaktapur',
+            employee: healthPost1.employee,
+            medicine: healthPost1.medicine,
+            gps_location: 'tyu89'
+        })
+
+        healthpost.save((err, healthpost) => {
+            chai.request(server)
+                .put('/healthpost/' + healthpost.id)
+                .send({
+                    name: "siddhi smriti",
+                    location: 'Bhaktapur',
+                    gps_location: 'xyz'
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    // res.body.should.have.property('message').eq('Healthpost information updated!');
+                    res.body.should.have.property('gps_location').eq('xyz');
+                    done();
+                });
+        });
+
+    });
+
+    it('It should DELETE all the records in the healthpost according to id', (done) => {
+        const healthpost = new HealthPost({
+            name: "siddhi smriti",
+            location: 'Bhaktapur',
+            gps_location: 'abc',
+            employee: healthPost1.employee,
+            medicine: healthPost1.medicine,
+            gps_location: 'xyz'
+        })
+        healthpost.save((err, book) => {
+            chai.request(server)
+                .delete('/healthpost/' + healthpost.id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        })
+
+    });
 
 })
 
