f2edafa6d866218e8059681f0125ffa6b3209e4c
karishma <kkunwar211@gmail.com>
2020-06-22 13:10:33 +0545

    Indentation and formating in orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -4,26 +4,25 @@ const mongoose = require('mongoose');
 const authenticate = require('../authenticate');
 const cors = require('./cors');
 
-const Orders= require('../models/orders');
+const Orders = require('../models/orders');
 const orderRouter = express.Router();
 
 
 orderRouter.use(bodyparser.json());
 
 orderRouter.route('/')
-  .options(cors.corsWithOptions, (req, res) => {
-    res.sendStatus(200);
-  })
+    .options(cors.corsWithOptions, (req, res) => {
+        res.sendStatus(200);
+    })
 
-  .get(cors.corsWithOptions, async (req, res, next) => {
+    .get(cors.corsWithOptions, async (req, res, next) => {
 
-  })
+    })
 
-  .post(cors.corsWithOptions,async(req,res,next)=>{
+    .post(cors.corsWithOptions, async (req, res, next) => {
 
-  })
+    })
 
-  .delete(cors.corsWithOptions,async(req,res,next)=>{
+    .delete(cors.corsWithOptions, async (req, res, next) => {
 
-  })
-  
+    })
