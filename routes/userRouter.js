7178768fa0258b1cd82e90667db2a3f248221d88
karishma <kkunwar211@gmail.com>
2020-07-15 13:04:15 +0545

    removed express-session from userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/users');
-const session = require('express-session');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const async = require('async');
